176d46b10642d3835a47158231711a5b11bb59a8
Patr0sss <kkiejno@gmail.com>
2023-11-05 15:37:58 +0100

    Security improvement

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,22 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "@firebase/auth";
-import firebase from "firebase/compat/app";
+// import firebase from "firebase/compat/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAOK8f00b_4iZtn03vSkI7j0r_037UaJxw",
-  authDomain: "aligato-shop.firebaseapp.com",
-  projectId: "aligato-shop",
-  storageBucket: "aligato-shop.appspot.com",
-  messagingSenderId: "246850830335",
-  appId: "1:246850830335:web:b83190ae3fc95f36d6f1ca",
-  measurementId: "G-0ZCG5SZ56T",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
